d20a9b280a493520cef1e0f0dcd38d0bcfee9792
vika728 <vika.sokolova728@gmail.com>
2021-03-15 01:22:46 +0600

    initial commit

---
+++ git_commands.py
@@ -0,0 +1,16 @@
+git init
+git remote add name url
+git pull
+git pull origin master
+git status
+git add
+git add name file
+git add.
+git commit
+git commit -m 'comment'
+git branch
+git branch name branch
+git checkout name branch
+git push
+git push origin master
+git reset filename
